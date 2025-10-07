2295d9fdbabeee8e3e4655ec38cd199a765e4177
SupphawitJantummee <s6812022611090@email.kmutnb.ac.th>
2025-10-07 15:27:42 +0700

    Add files via upload

---
+++ Sec-2_6812022611090_Supphawit.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int time ;
+    float base_fee_toal_fee;
+    char service;
+    int O = [15];
+    int B = [40];
+    int A = [20];
+
+    printf("ประเภทช่องทางธุรกรรม:\n")
+    printf("ประเภท O ออนไลน์ ช 15 \n")
+    printf("ประเภท B ที่สาขา = 40 บาท \n")
+    printf("ประเภท A ตู้ ATM 20 บาท \n")
+}
+    printf("กรอกประเภทอัตราส่วนของค่าธรรมเนียม O,B,A")
+    scanf("%c", &service);
+
+    printf("กรอกจำนวนระหว่าง 3-12"):");
+    scanf("%d", &time);
+
+    if(times< 3, times >12){
+    printf("จำนวนครั้งที่ต้องอยู่ระหว่าง 3-12 \n")
+    return 0;
+}
