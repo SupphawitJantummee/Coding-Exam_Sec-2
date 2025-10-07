c095e5daaeaee5d9f8284c25c8be4f5fd5dbd8d0
SupphawitJantummee <s6812022611090@email.kmutnb.ac.th>
2025-10-07 15:25:19 +0700

    Add files via upload

---
+++ Sec-2_6812022611090_Supphawit.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int n = 3-12 ;
+    int o = [15] ;
+    int b = [40] ;
+    int a = [20] ;
+
+    printf("Enter your number 3-12");
+    scanf("%n",number);
+    printf("รับข้อมูลประเภทช่องทางทางธุรกรรม")
+    printf("o = %d\n",o)")
+}
